7314c5e8300e50d9c5f8c44f7eabd34894859224
yakreb <berkayturna@gmail.com>
2024-02-16 15:28:23 +0300

    spread kullanımı

---
+++ spread.js
@@ -0,0 +1,57 @@
+//Array birleştirme
+
+// const arr1 = [1, 2, 3];
+// const arr2 = [4, 5, 6];
+
+// const combinedArr = [...arr1, ...arr2];
+
+// console.log(combinedArr);
+
+
+//Object birleştirme
+
+// const obj1 = {
+//   name: "John",
+//   age: 30,
+// };
+
+// const obj2 = {
+//   city: "London",
+//   country: "England",
+// };
+
+// const combinedObj = { ...obj1, ...obj2 };
+
+// console.log(combinedObj);
+
+
+//Fonksiyona argüman geçirme
+
+// function sum(a, b, c) {
+//   return a + b + c;
+// }
+
+// const numbers = [1, 2, 3];
+
+// const sumOfNumbers = sum(...numbers);
+
+// console.log(sumOfNumbers);
+
+
+//Diziden kopya almak
+
+// const arr = [1, 2, 3];
+
+// const copyArr = [...arr];
+
+// console.log(copyArr);
+
+
+//Dizinin kalanını yakalama
+
+// const arr = [1, 2, 3, 4, 5];
+
+// const [first, ...rest] = arr;
+
+// console.log(first);
+// console.log(rest);
