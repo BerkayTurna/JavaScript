a6b679ab7cc11b369f805ad580f276e0f9dbfcd6
yakreb <berkayturna@gmail.com>
2024-03-02 19:39:54 +0300

    some, every, find, finfIndex, map, sort

--- array-methods.js
+++ array-methods.js
@@ -1,8 +1,203 @@
+// const students = [
+//   {
+//     fullName: "Berkay Turna",
+//     age: 25,
+//     isDeleted: false,
+//     isActive: true,
+//     courses: ["JavaScript", "React", "Node.js"],
+//     instructors: [
+//       { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
+//       { fullName: "Hicran Ertugral", topics: ["TypeScript", "Angular"] },
+//     ],
+//   },
+//   {
+//     fullName: "Cenk Grid Kaynak",
+//     age: 32,
+//     isDeleted: false,
+//     isActive: true,
+//     courses: ["JavaScript", "React Native", ".Net Core"],
+//     instructors: [
+//       { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
+//       { fullName: "Korhan Kosece", topics: [".Net Core"] },
+//     ],
+//   },
+//   {
+//     fullName: "Ali Riza Taskiran",
+//     age: 24,
+//     isDeleted: false,
+//     isActive: true,
+//     courses: ["Java", "Golang"],
+//     instructors: [
+//       { fullName: "Akin Kaldiroglu", topics: ["Spring", "C++"] },
+//       { fullName: "Buse Seker", topics: ["Problem Solving"] },
+//     ],
+//   },
+//   {
+//     fullName: "Mert Kirant",
+//     age: 32,
+//     isDeleted: false,
+//     isActive: true,
+//     courses: ["Expressjs", "Node.js"],
+//     instructors: [
+//       { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
+//       { fullName: "Onder Tarım", topics: ["React Native"] },
+//     ],
+//   },
+//   {
+//     fullName: "Firat Can Tas",
+//     age: 26,
+//     isDeleted: false,
+//     isActive: false,
+//     courses: ["Expressjs", "Node.js", ".Net Core"],
+//     instructors: [
+//       { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
+//       { fullName: "Onder Tarım", topics: ["React Native"] },
+//     ],
+//   },
+//   {
+//     fullName: "Baris Peker",
+//     age: 38,
+//     isDeleted: true,
+//     isActive: false,
+//     courses: ["JavaScript", "HTML"],
+//     instructors: [
+//       { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
+//       { fullName: "Onder Tarım", topics: ["React Native"] },
+//     ],
+//   },
+// ];
+
+// //forEach
+
+// const result = students.forEach((student, index) =>
+//   console.log(`coursesIndex ${index}: <${student.fullName}> ${student.courses}`)
+// );
+
+// const result2 = students.forEach((student, index) =>
+//   console.log(`${index} ${student.isDeleted}`)
+// );
+
+// //map
+
+// const newStu = students.map((student) => student.courses);
+
+// console.log(newStu.flat());
+
+// const newStd = students.map((stu) => stu.age);
+// console.log(newStd);
+
+// //filter
+
+// console.log(
+//   students.filter((student) => !student.isDeleted && student.isActive)
+// );
+
+// const students = [
+//   {
+//     fullName: "Berkay Turna",
+//     age: 25,
+//     isDeleted: false,
+//     score: 90,
+//     isActive: true,
+//     courses: ["JavaScript", "React", "Node.js"],
+//     instructors: [
+//       { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
+//       { fullName: "Hicran Ertugral", topics: ["TypeScript", "Angular"] },
+//     ],
+//   },
+//   {
+//     fullName: "Cenk Grid Kaynak",
+//     age: 32,
+//     isDeleted: false,
+//     score: 85,
+//     isActive: true,
+//     courses: ["JavaScript", "React Native", ".Net Core"],
+//     instructors: [
+//       { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
+//       { fullName: "Korhan Kosece", topics: [".Net Core"] },
+//     ],
+//   },
+//   {
+//     fullName: "Ali Riza Taskiran",
+//     age: 24,
+//     isDeleted: false,
+//     score: 92,
+//     isActive: true,
+//     courses: ["Java", "Golang"],
+//     instructors: [
+//       { fullName: "Akin Kaldiroglu", topics: ["Spring", "C++"] },
+//       { fullName: "Buse Seker", topics: ["Problem Solving"] },
+//     ],
+//   },
+//   {
+//     fullName: "Mert Kirant",
+//     age: 23,
+//     isDeleted: false,
+//     score: 88,
+//     isActive: true,
+//     courses: ["Expressjs", "Node.js"],
+//     instructors: [
+//       { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
+//       { fullName: "Onder Tarım", topics: ["React Native"] },
+//     ],
+//   },
+//   {
+//     fullName: "Firat Can Tas",
+//     age: 24,
+//     isDeleted: false,
+//     score: 60,
+//     isActive: false,
+//     courses: ["Expressjs", "Node.js", ".Net Core"],
+//     instructors: [
+//       { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
+//       { fullName: "Onder Tarım", topics: ["React Native"] },
+//     ],
+//   },
+//   {
+//     fullName: "Baris Peker",
+//     age: 38,
+//     isDeleted: true,
+//     score: 75,
+//     isActive: false,
+//     courses: ["JavaScript", "HTNL"],
+//     instructors: [
+//       { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
+//       { fullName: "Onder Tarım", topics: ["React Native"] },
+//     ],
+//   },
+// ];
+
+// const isAnyScoreOver82 = students.some((student) => student.score > 82);
+// const isAnyScoreOver95 = students.some((student) => {
+//   return student.score > 95;
+// });
+
+// console.log(isAnyScoreOver82, isAnyScoreOver95);
+
+// const isEveryStudentActive = students.every(
+//   (student) => student.isActive === true
+// );
+
+// console.log(isEveryStudentActive);
+
+// const whereIsBerkay = students.find(
+//   (student) => student.fullName === "Berkay Turna"
+// );
+
+// console.log(whereIsBerkay);
+
+// const indexOfBerkay = students.findIndex(
+//   (student) => student.fullName === "Berkay Turna"
+// );
+
+// console.log(indexOfBerkay);
+
 const students = [
   {
     fullName: "Berkay Turna",
     age: 25,
     isDeleted: false,
+    score: 90,
     isActive: true,
     courses: ["JavaScript", "React", "Node.js"],
     instructors: [
@@ -14,6 +209,7 @@ const students = [
     fullName: "Cenk Grid Kaynak",
     age: 32,
     isDeleted: false,
+    score: 85,
     isActive: true,
     courses: ["JavaScript", "React Native", ".Net Core"],
     instructors: [
@@ -22,9 +218,34 @@ const students = [
     ],
   },
   {
-    fullName: "Ali Riza Taskiran",
+    fullName: "ali Riza Taskiran",
     age: 24,
     isDeleted: false,
+    score: 92,
+    isActive: true,
+    courses: ["Java", "Golang"],
+    instructors: [
+      { fullName: "Akin Kaldiroglu", topics: ["Spring", "C++"] },
+      { fullName: "Buse Seker", topics: ["Problem Solving"] },
+    ],
+  },
+  {
+    fullName: "Hakan Berke Temur",
+    age: 24,
+    isDeleted: false,
+    score: 92,
+    isActive: true,
+    courses: ["Java", "Golang"],
+    instructors: [
+      { fullName: "Akin Kaldiroglu", topics: ["Spring", "C++"] },
+      { fullName: "Buse Seker", topics: ["Problem Solving"] },
+    ],
+  },
+  {
+    fullName: "Barkin Sayin",
+    age: 22,
+    isDeleted: false,
+    score: 92,
     isActive: true,
     courses: ["Java", "Golang"],
     instructors: [
@@ -34,8 +255,21 @@ const students = [
   },
   {
     fullName: "Mert Kirant",
-    age: 32,
+    age: 23,
     isDeleted: false,
+    score: 88,
+    isActive: true,
+    courses: ["Expressjs", "Node.js"],
+    instructors: [
+      { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
+      { fullName: "Onder Tarım", topics: ["React Native"] },
+    ],
+  },
+  {
+    fullName: "Onur Ege Erkek",
+    age: 23,
+    isDeleted: false,
+    score: 88,
     isActive: true,
     courses: ["Expressjs", "Node.js"],
     instructors: [
@@ -45,10 +279,23 @@ const students = [
   },
   {
     fullName: "Firat Can Tas",
-    age: 26,
+    age: 24,
     isDeleted: false,
+    score: 91,
     isActive: false,
     courses: ["Expressjs", "Node.js", ".Net Core"],
+    instructors: [
+      { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
+      { fullName: "Onder Tarım", topics: ["React Native"] },
+    ],
+  },
+  {
+    fullName: "Ceren Dincer",
+    age: 27,
+    isDeleted: false,
+    score: 75,
+    isActive: true,
+    courses: ["JavaScript", "HTNL"],
     instructors: [
       { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
       { fullName: "Onder Tarım", topics: ["React Native"] },
@@ -58,36 +305,23 @@ const students = [
     fullName: "Baris Peker",
     age: 38,
     isDeleted: true,
+    score: 75,
     isActive: false,
-    courses: ["JavaScript", "HTML"],
+    courses: ["JavaScript", "HTNL"],
     instructors: [
       { fullName: "Orkun Durmaz", topics: ["JavaScript", "React"] },
       { fullName: "Onder Tarım", topics: ["React Native"] },
     ],
   },
 ];
+const lowerCaseStudentNames = students.map((student)=>student.fullName.toLowerCase());
 
-//forEach 
+console.log(lowerCaseStudentNames);   
 
-const result = students.forEach((student, index) =>
-  console.log(`coursesIndex ${index}: <${student.fullName}> ${student.courses}`)
-);
+const sortStudentsByName = students.sort((a,b)=>a.fullName>b.fullName ? 1 : -1);
 
-const result2 = students.forEach((student, index) =>
-  console.log(`${index} ${student.isDeleted}`)
-);
+console.log(sortStudentsByName);
 
-//map
+// const sortStudentsByScore = students.sort((a,b)=> (a.score-b.score));
 
-const newStu = students.map((student) => student.courses);
-
-console.log(newStu.flat());
-
-const newStd = students.map((stu) => stu.age);
-console.log(newStd);
-
-//filter
-
-console.log(
-  students.filter((student) => !student.isDeleted && student.isActive)
-);
+// console.log(sortStudentsByScore);
