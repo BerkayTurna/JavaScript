fb76bf75ff64e4975ff6109f15c8ef931afaadfc
yakreb <berkayturna@gmail.com>
2024-03-13 15:55:34 +0300

    hesap makinesi deneme

--- Test/freecodecamp.js
+++ Test/freecodecamp.js
@@ -1,83 +1,108 @@
-//Record Collection
+// //Record Collection
 
-const recordCollection = {
-    2548: {
-      albumTitle: 'Slippery When Wet',
-      artist: 'Bon Jovi',
-      tracks: ['Let It Rock', 'You Give Love a Bad Name']
-    },
-    2468: {
-      albumTitle: '1999',
-      artist: 'Prince',
-      tracks: ['1999', 'Little Red Corvette']
-    },
-    1245: {
-      artist: 'Robert Palmer',
-      tracks: []
-    },
-    5439: {
-      albumTitle: 'ABBA Gold'
-    }
-  };
+// const recordCollection = {
+//     2548: {
+//       albumTitle: 'Slippery When Wet',
+//       artist: 'Bon Jovi',
+//       tracks: ['Let It Rock', 'You Give Love a Bad Name']
+//     },
+//     2468: {
+//       albumTitle: '1999',
+//       artist: 'Prince',
+//       tracks: ['1999', 'Little Red Corvette']
+//     },
+//     1245: {
+//       artist: 'Robert Palmer',
+//       tracks: []
+//     },
+//     5439: {
+//       albumTitle: 'ABBA Gold'
+//     }
+//   };
   
-  function updateRecords(records, id, prop, value) {
+//   function updateRecords(records, id, prop, value) {
   
-    if (value) {
-      if (prop === "tracks") {
-        records[id][prop] = records[id][prop] || [];
-        value && records[id][prop].push(value);
-      } else {
-        records[id][prop] = value;
-      }
-    }
-    else delete records[id][prop];
+//     if (value) {
+//       if (prop === "tracks") {
+//         records[id][prop] = records[id][prop] || [];
+//         value && records[id][prop].push(value);
+//       } else {
+//         records[id][prop] = value;
+//       }
+//     }
+//     else delete records[id][prop];
   
-    return records;
+//     return records;
+//   }
+  
+//   updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+
+
+
+//   //Profile Lookup
+
+// const contacts = [
+//     {
+//       firstName: "Akira",
+//       lastName: "Laine",
+//       number: "0543236543",
+//       likes: ["Pizza", "Coding", "Brownie Points"],
+//     },
+//     {
+//       firstName: "Harry",
+//       lastName: "Potter",
+//       number: "0994372684",
+//       likes: ["Hogwarts", "Magic", "Hagrid"],
+//     },
+//     {
+//       firstName: "Sherlock",
+//       lastName: "Holmes",
+//       number: "0487345643",
+//       likes: ["Intriguing Cases", "Violin"],
+//     },
+//     {
+//       firstName: "Kristian",
+//       lastName: "Vos",
+//       number: "unknown",
+//       likes: ["JavaScript", "Gaming", "Foxes"],
+//     },
+//   ];
+  
+//   function lookUpProfile(name, prop) {
+//     // Only change code below this line
+//   for(let i=0; i<contacts.length; i++){
+//     if(name===contacts[i].firstName){
+//       if(contacts[i].hasOwnProperty(prop)){
+//         return contacts[i][prop];
+//       }else{
+//         return "No such property";
+//       }
+//     }
+//   }
+//   return "No such contact";
+//   }
+
+  // Explicit constructor
+class SpaceShuttle {
+  constructor(targetPlanet) {
+    this.targetPlanet = targetPlanet;
   }
-  
-  updateRecords(recordCollection, 5439, 'artist', 'ABBA');
-
-
-
-  //Profile Lookup
-
-const contacts = [
-    {
-      firstName: "Akira",
-      lastName: "Laine",
-      number: "0543236543",
-      likes: ["Pizza", "Coding", "Brownie Points"],
-    },
-    {
-      firstName: "Harry",
-      lastName: "Potter",
-      number: "0994372684",
-      likes: ["Hogwarts", "Magic", "Hagrid"],
-    },
-    {
-      firstName: "Sherlock",
-      lastName: "Holmes",
-      number: "0487345643",
-      likes: ["Intriguing Cases", "Violin"],
-    },
-    {
-      firstName: "Kristian",
-      lastName: "Vos",
-      number: "unknown",
-      likes: ["JavaScript", "Gaming", "Foxes"],
-    },
-  ];
-  
-  function lookUpProfile(name, prop) {
-    // Only change code below this line
-  for(let i=0; i<contacts.length; i++){
-    if(name===contacts[i].firstName){
-      if(contacts[i].hasOwnProperty(prop)){
-        return contacts[i][prop];
-      }else{
-        return "No such property";
-      }
-    }
+  takeOff() {
+    console.log("To " + this.targetPlanet + "!");
   }
-  return "No such contact";
+}
+
+// Implicit constructor 
+class Rocket {
+  launch() {
+    console.log("To the moon!");
   }
+}
+
+const zeus = new SpaceShuttle('Jupiter');
+// prints To Jupiter! in console
+zeus.takeOff();
+
+const atlas = new Rocket();
+// prints To the moon! in console
+atlas.launch();
