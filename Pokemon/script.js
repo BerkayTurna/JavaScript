1a782e129303bfdf6c8380ebfce257f0051f71f6
yakreb <berkayturna@gmail.com>
2024-03-22 23:09:24 +0300

    about to finish

--- Pokemon/script.js
+++ Pokemon/script.js
@@ -13,26 +13,24 @@ const getPokemonById = async (pokemonId) => {
 
 //pokemon ile ilgili istenen parametreleri container oluşturup içine yazdırma
 const createPokemonCard = (pokemonData) => {
-    // console.log(pokemonData.sprites.other.dream_world.front_default);
-    // console.log(pokemonData.id);
-    // console.log(pokemonData.name);
-    // pokemonData.types.forEach(
-    //     (type) => console.log(type.type.name)
-    // )
-
+    
     const cardContainer = document.createElement("div");
     cardContainer.classList.add("pokemon-card");
+
     const pokemonImg = document.createElement("img");
     pokemonImg.src = pokemonData.sprites.other.dream_world.front_default;
     cardContainer.appendChild(pokemonImg);
+
     const pokemonNumName = document.createElement("h2");
-    pokemonNumName.textContent = `#${pokemonData.id} ${pokemonData.name}`;
+    pokemonNumName.textContent = `${pokemonData.id} - ${pokemonData.name}`;
     cardContainer.appendChild(pokemonNumName)
+
     const typesContainer = document.createElement("div");
     const typesTag = document.createElement("h3");
     typesTag.textContent = "Types:";
     typesContainer.appendChild(typesTag);
     typesContainer.classList.add("pokemon-types");
+    
     pokemonData.types.forEach(
         (type) => {
             const pokemonType = document.createElement("ul");
@@ -44,55 +42,76 @@ const createPokemonCard = (pokemonData) => {
     cardContainer.appendChild(typesContainer);
     document.getElementById("pokemon-card-container").appendChild(cardContainer);
 };
-//
-//istenen limitlerle tüm pokemonları çekme fonksiyonu
-const getAllPokemon = async (limit = 0, offset = 0) => {
-    const res = await fetch(`${pokemonApiURL}pokemon?limit=${limit}&offset=${offset}`);
-    const data = await res.json();
-    console.log(data)
-    return data;
-};
 
-// getAllPokemon()
-//     .then(allPokemon => {
-//         console.log(allPokemon)});
-
-// HTML template nasıl olur?
-// döngüde asenkron nasıl çalışır???
+//Tüm pokemonları gösterme fonksiyonu
 const displayAllPokemon = async (limit = 151, offset = 0) => {
     try {
-        const allPokemonData = await getAllPokemon(limit, offset);
-        const pokemonList = allPokemonData.results;
-        console.log(pokemonList);
+        const allPokemonData = await fetch(
+            `${pokemonApiURL}pokemon?limit=${limit}&offset=${offset}`
+        );
+        const data = await allPokemonData.json();
+        const pokemonList = data.results;
 
-        pokemonList.forEach((pokemon) => {
-            getPokemonById(pokemon.name)
-                .then((pokemonData) => createPokemonCard(pokemonData))
-        });
-    } catch (error) {
+        for (const pokemon of pokemonList) {
+            try {
+                const pokemonRes = await fetch(
+                    `${pokemonApiURL}pokemon/${pokemon.name}`
+                );
+                const pokemonData = await pokemonRes.json();
+                createPokemonCard(pokemonData);
+            }
+            catch (error) {
+                console.log(error);
+            }
+        };
+    }
+    catch (error) {
         console.log(error);
     }
 };
 
 displayAllPokemon();
 
+//filtreleme ile name search yapma
+const filterPokemon = () => {
+    const pokemonInput = document.getElementById("input-name").value.toLowerCase();
+    const selectAllPokemon = document.querySelectorAll(".pokemon-card");
+    selectAllPokemon.forEach((pokemon) => {
+        const pokemonName = pokemon.querySelector("h2").textContent; //.toLowercase();
+        const isPokemonInclude = pokemonName.includes(pokemonInput);
+        if (isPokemonInclude) {
+            pokemon.style.display = "block";
+        } else {
+            pokemon.style.display = "none";
+        }
+    });
+}
+document.getElementById("input-name").addEventListener("input", filterPokemon)
 
-//
-// getPokemonById(2)
-//     .then(pokemon => {
-//         createPokemonCard(pokemon)
-//     });
 
-// fetch(`${pokeApiURL}pokemon/1`)
-//     .then(response => {
-//         if (!response.ok) {
-//             throw new Error("response not ok");
-//         }
-//         return response.json();
-//     })
-//     .then(data => {
-//         console.log(data);
-//     })
-//     .catch(error => {
-//         console.error("fetch problemi: ", error);
-//     })
+//filtreleme ile type search yapma
+const filterByType = ()=>{
+    const searchInput = document.getElementById("input-type").value.toLowerCase();
+    const selectAllPokemon = document.querySelectorAll(".pokemon-card");
+
+    //type search sırasında contentin tekrar gelmesi için
+    if (!searchInput.trim()) {
+        selectAllPokemon.forEach((card) => {
+          card.style.display = "block";
+        });
+        return;
+      };
+
+    selectAllPokemon.forEach((pokemon)=>{
+    const pokemonTypes = [...pokemon.querySelectorAll(".type-name")].map((type)=>
+    type.textContent.toLowerCase());
+    const searchId = pokemonTypes.includes(searchInput);
+    if(searchId){
+        pokemon.style.display = "block";
+    }else{
+        pokemon.style.display = "none";
+    }
+});
+};
+
+document.getElementById("input-type").addEventListener("input", filterByType);
