a851e0d442d98d2eeab34b6ce9bd49b73d0bcf71
yakreb <berkayturna@gmail.com>
2024-03-31 12:48:05 +0300

    useState update

--- react project/student-manager/src/App.js
+++ react project/student-manager/src/App.js
@@ -2,100 +2,80 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  // let studentName1 = "Barkın Onay Sayın";
-  // let studentName2 = "Mert Ders Kırant";
-  // let studentName3 = "Onur Ege Ercallbeck";
-
-  //hooks
-  //useState ekranı yeniden yazdırır
-  // console.log(useState("Cenk Grid Kaynak"));
-
-  //destructuring ile...
-  // const [studentName1, setStudentName1] = useState("Barkın Onay Sayın");
-  // const [studentName2, setStudentName2] = useState("Mert Ders Kırant");
-  // const [studentName3, setStudentName3] = useState("Onur Ege Ercallbeck");
 
   //data state
-  const [name, setName] = useState("");
-  const [course, setCourse] = useState("");
-  const [instructor, setInstructor] = useState("");
-
   //Liste oluşturma hooku
   const [studentsList, setStudentsList] = useState([]);
 
   //input state
-  const [nameInput, setNameInput] = useState("");
-  const [courseInput, setCourseInput] = useState("");
-  const [instructorInput, setInstructorInput] = useState("");
+  const [student, setStudent] = useState({ name: "", course: "", instructor: "" })
 
   //error state
-  const [nameError, setNameError] = useState(false);
-  const [courseError, setCourseError] = useState(false);
-  const [instructorError, setInstructorError] = useState(false);
 
+  const [allError, setAllError] = useState(
+    {
+      name: false,
+      course: false,
+      instructor: false,
+    }
+  )
+
+  const addStudent = (event) => {
+    event.preventDefault();
+
+    //Hatalı giriş olmadığını varsayarak başlama
+    //Hataları silme
+    setAllError({
+      name: false,
+      course: false,
+      instructor: false,
+    });
+
+
+    if (!student.name.trim() || !student.course.trim() || !student.instructor.trim()) {
+      //Error varsa yazdırma
+      setAllError(
+        {
+          name: !student.name.trim(),
+          course: !student.course.trim(),
+          instructor: !student.instructor.trim(),
+        }
+      );
+    }
+
+    //studentsList güncelleme
+    setStudentsList(
+      [
+        ...studentsList,
+        {
+          ...student,
+          id: Math.random().toString()
+        }
+      ]
+    );
+    //input içeriğini temizleme - two way binding
+    setStudent({ name: "", course: "", instructor: "" });
+
+  }
   return (
     /* JSX */
     <div className="app">
       <h2>Student Manager</h2>
       <h3>Enter Data</h3>
       <form action="" className="student-form" >
-        <input type="text" placeholder="student name" value={nameInput}
-          onChange={(event) => setNameInput(event.target.value)} />
+        <input type="text" placeholder="student name" value={student.name}
+          onChange={(event) => setStudent({ ...student, name: event.target.value })} />
         {/*nameError koşulu meydana geldiğinde string yazma - conditional rendering*/}
-        {nameError && <p>İsim alanı boş olamaz!</p>}
-        <input type="text" placeholder="course" value={courseInput}
-          onChange={(event) => setCourseInput(event.target.value)} />
-        {courseError && <p>Course alanı boş olamaz!</p>}
-        <input type="text" placeholder="instructor" value={instructorInput}
-          onChange={(event) => setInstructorInput(event.target.value)} />
-        {instructorError && <p>Instructor alanı boş olamaz!</p>}
+        {allError.name && <p>İsim alanı boş olamaz!</p>}
+        <input type="text" placeholder="course" value={student.course}
+          onChange={(event) => setStudent({ ...student, course: event.target.value })} />
+        {allError.course && <p>Course alanı boş olamaz!</p>}
+        <input type="text" placeholder="instructor" value={student.instructor}
+          onChange={(event) => setStudent({ ...student, instructor: event.target.value })} />
+        {allError.instructor && <p>Instructor alanı boş olamaz!</p>}
         <input
           type="submit"
-          onClick={
-            (event) => {
-              event.preventDefault();
-
-              //Hatalı giriş olmadığını varsayarak başlama
-              //Hataları silme
-              setNameError(false);
-              setCourseError(false);
-              setInstructorError(false);
-
-
-              if (!nameInput.trim() || !courseInput.trim() || !instructorInput.trim()) {
-                // koşul sağlanıyorsa ve Error varsa yazdırma
-                !nameInput.trim() && setNameError(true);
-                !courseInput.trim() && setCourseError(true);
-                !instructorInput.trim() && setInstructorError(true);
-              } else {
-
-                setName(nameInput.trim());
-                setCourse(courseInput.trim());
-                setInstructor(instructorInput.trim());
-                setStudentsList(
-                  [
-                    ...studentsList,
-                    {
-                      name: nameInput,
-                      course: courseInput,
-                      instructor: instructorInput,
-                      id: Math.random().toString(),                      
-                    }
-                  ]
-                )
-
-                //input içeriğini temizleme
-                setNameInput("");
-                setCourseInput("");
-                setInstructorInput("");
-
-                //DOM DOM Kurşunu
-                // document.getElementById("nameInputId").value = "";
-                // document.getElementById("courseInputId").value = "";
-                // document.getElementById("instructorId").value = "";
-              };
-            }
-          } />
+          onClick={addStudent} />
       </form>
       <div className="student-list">
         <div className="student-card">
