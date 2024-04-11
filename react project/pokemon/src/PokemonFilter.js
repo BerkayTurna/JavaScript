8736bb6eb3258dc72b4dfe628e3548364fc97d7c
yakreb <berkayturna@gmail.com>
2024-04-11 18:50:23 +0300

    App.js import correction, useEffect for list all pokemons when page loaded

--- react project/pokemon/src/PokemonFilter.js
+++ react project/pokemon/src/PokemonFilter.js
@@ -1,51 +1,60 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function PokemonFilter() {
-
+    //useState'ler ile listeleme, filtrelemeler
     const [pokemonList, setPokemonList] = useState([]);
     const [filteredPokemonList, setFilteredPokemonList] = useState([]);
     const [nameFilter, setNameFilter] = useState('');
     const [typeFilter, setTypeFilter] = useState('');
 
     const pokemonApiURL = 'https://pokeapi.co/api/v2/';
+    //ilk sayfa yüklenmesinde tüm listeyi getirir.
+    useEffect(() => {
+        const getPokemon = async () => {
+            try {
+                const res = await fetch(`${pokemonApiURL}pokemon?limit=151`);
+                const data = await res.json();
+                const promises = data.results.map(async pokemon => {
+                    const res = await fetch(pokemon.url);
+                    const pokemonData = await res.json();
+                    return {
+                        id: pokemonData.id,
+                        name: pokemonData.name,
+                        image: pokemonData.sprites.other['official-artwork'].front_default,
+                        types: pokemonData.types.map(type => type.type.name)
+                    };
+                });
+                const pokemonDetail = await Promise.all(promises);
+                setPokemonList(pokemonDetail);
+                setFilteredPokemonList(pokemonDetail);
+            } catch (error) {
+                console.error('Error fetching Pokemon data:', error);
+            }
+        };
 
-    const getPokemonById = async () => {
-        try {
-            const res = await fetch(`${pokemonApiURL}pokemon`);
-            const data = await res.json();
-            const promises = data.results.map(async pokemon => {
-                const res = await fetch(pokemon.url);
-                const pokemonData = await res.json();
-                return {
-                    id: pokemonData.id,
-                    name: pokemonData.name,
-                    image: pokemonData.sprites.other['official-artwork'].front_default,
-                    types: pokemonData.types.map(type => type.type.name)
-                };
-            });
-            const pokemonDetail = await Promise.all(promises);
-            setPokemonList(pokemonDetail);
-            setFilteredPokemonList(pokemonDetail);
-        } catch (error) {
-            console.error('Error fetching Pokemon data:', error);
-        }
-    };
+        getPokemon();
+    }, []);
+
     const filterPokemon = () => {
         let filtered = pokemonList;
         if (nameFilter) {
             filtered = filtered.filter(pokemon =>
-                pokemon.name.toLowercase().includes(nameFilter.toLowerCase())
+                pokemon.name.toLowerCase().includes(nameFilter.toLowerCase())
             );
         }
         if (typeFilter) {
-            filtered = filtered.filter(async pokemon => {
-                const res = await fetch(pokemon.url);
-                const data = await res.json();
-                return data.types.some(type => type.type.name.toLowerCase() === typeFilter.toLowerCase());
-            });
-        }
+            filtered = filtered.filter(pokemon =>
+                pokemon.types.includes(typeFilter.toLowerCase())
+            )
+        };
+
         setFilteredPokemonList(filtered);
     };
+
+    //useEffect ile her değişiklik olduğunda tekrar filtreleme sağlanır.
+    useEffect(() => {
+        filterPokemon();
+    }, [nameFilter, typeFilter]);
 
     const handleNameInputChange = event => {
         setNameFilter(event.target.value);
@@ -70,7 +79,7 @@ function PokemonFilter() {
                 value={typeFilter}
                 onChange={handleTypeInputChange}
             />
-            <button onClick={getPokemonById}>Pokemon Getir</button>
+            {/* <button onClick={getPokemon}>Pokemon Getir</button> */}
             <div>
                 {filteredPokemonList.map((pokemon, index) => (
                     <div key={index}>
