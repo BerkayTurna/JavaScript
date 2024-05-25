680f1da45adc7bf414b0e9b873809d2ad99450d1
yakreb <berkayturna@gmail.com>
2024-05-25 16:27:48 +0300

    solution with setTimeout and clearTimeout

--- react project/student-manager-vite/src/pages/shared/not-found-page/NotFoundPage.jsx
+++ react project/student-manager-vite/src/pages/shared/not-found-page/NotFoundPage.jsx
@@ -1,27 +1,39 @@
 import { useEffect, useState } from "react";
-import { redirect, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const NotFoundPage = () => {
   const [time, setTime] = useState(5);
-  const navigate = useNavigate();
+  const navigate = useNavigate(); // bileşen içerisinde çağrıldığında parametre olarak aldığı URL'ye gitmemizi sağlar.
 
-  //   const timer = () => {
-  //     if (time > 0) {
-  //       setTime(time - 1);
-  //     }
-  //     return console.log(time);
-  //   };
+  //   useEffect(() => { //setInterval fonksiyonu 1000 değeri vermemiz dolayısıyla saniyede bir çalışır.
+  //     const countdown = setInterval(() => {
+  //       setTime((prevTime) => {
+  //         if (prevTime === 0) {
+  //           clearInterval(countdown); //setIntervali durdurmak için kullanılır.
+  //           navigate("/");
+  //         }
+  //         return prevTime - 1;
+  //       });
+  //     }, 1000);
+  //     return () => clearInterval(countdown);
+  //   }, [navigate]);
+  //Yukarıdaki girişimde NotFoundPage renderlama yaparken BrowserRouter update
+  //edilemiyor.
 
+  //Aşağıdaki girişimde time 0'dan büyük olduğu sürece setTimeout ile saniyede
+  //bir işlem yapılırken clearTimeout zamanlayıcıyı durdurur. time 0'a geldiğinde
+  //else devreye girerek useNavigate'i çağırıp anasayfaya döndürür.
   useEffect(() => {
-    const countdown = setTimeout(() => {
-      setTime((prevTime) => {
-        return prevTime - 1;
-      });
-      if (time === 0) {
-        navigate("/");
-      }
-    }, 1000);
-  }, []);
+    if (time > 0) {
+      const countdown = setTimeout(() => {
+        setTime((prevTime) => prevTime - 1);
+      }, 1000);
+      return () => clearTimeout(countdown); //syntax clearTimeout(setTimeoutId) şeklindedir.
+    } else {
+      navigate("/");
+    }
+  }, [time, navigate]); // useEffect time her değiştiğinde yeniden çalışır ve
+  //time=0 olduğunda navigate çalışır. Bu nedenle her iki dependency yazılmalıdır.
   return (
     <div>
       <h1>OOoopps...</h1>
