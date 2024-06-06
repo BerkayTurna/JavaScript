43c05a2f3c57b01010450e573c33c31fafc88fcf
yakreb <berkayturna@gmail.com>
2024-06-06 21:19:19 +0300

    encapsulated functions for buttons

--- react project/student-manager-vite/src/pages/app/reducer-test-page/ReducerPage.jsx
+++ react project/student-manager-vite/src/pages/app/reducer-test-page/ReducerPage.jsx
@@ -21,13 +21,6 @@ const reducer = (state, action) => {
         lastUpdatedAt: new Date().toLocaleString(),
       };
     case "INCREMENT":
-      if (isNaN(action.payload.amount)) {
-        return {
-          ...state,
-          lastUpdatedAt: new Date().toLocaleString(),
-          error: "Not a number",
-        };
-      }
       return {
         ...state,
         counter: state.counter + action.payload.amount,
@@ -35,13 +28,6 @@ const reducer = (state, action) => {
         error: null,
       };
     case "DECREMENT":
-      if (isNaN(action.payload.amount)) {
-        return {
-          ...state,
-          lastUpdatedAt: new Date().toLocaleString(),
-          error: "Not a number",
-        };
-      }
       return {
         ...state,
         counter: state.counter - action.payload.amount,
@@ -49,13 +35,6 @@ const reducer = (state, action) => {
         error: null,
       };
     case "SET_COUNTER":
-      if (isNaN(action.payload.amount)) {
-        return {
-          ...state,
-          lastUpdatedAt: new Date().toLocaleString(),
-          error: "Not a number",
-        };
-      }
       return {
         ...state,
         counter: action.payload.amount,
@@ -67,6 +46,13 @@ const reducer = (state, action) => {
         ...state,
         counter: 0,
         lastUpdatedAt: new Date().toLocaleString(),
+        error: null,
+      };
+    case "ERROR":
+      return {
+        ...state,
+        lastUpdatedAt: new Date().toLocaleString(),
+        error: action.payload.error,
       };
     default:
       return state;
@@ -78,6 +64,57 @@ const numRef = createRef();
 
 const ReducerPage = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  const checkIncrement = () => {
+    if (isNaN(incRef.current.value) || incRef.current.value === "") {
+      dispatch({
+        type: "ERROR",
+        payload: { error: "Increment value must be a number" },
+      });
+    } else {
+      dispatch({
+        type: "INCREMENT",
+        payload: { amount: Number(incRef.current.value) },
+      });
+      incRef.current.value = "";
+    }
+  };
+
+  const checkDecrement = () => {
+    if (isNaN(decRef.current.value) || decRef.current.value === "") {
+      dispatch({
+        type: "ERROR",
+        payload: { error: "Decrement value must be a number" },
+      });
+    } else {
+      dispatch({
+        type: "DECREMENT",
+        payload: { amount: Number(decRef.current.value) },
+      });
+      decRef.current.value = "";
+    }
+  };
+  const checkSet = () => {
+    if (isNaN(numRef.current.value) || numRef.current.value === "") {
+      dispatch({
+        type: "ERROR",
+        payload: { error: "Set Number value must be a number" },
+      });
+    } else {
+      dispatch({
+        type: "SET_COUNTER",
+        payload: { amount: Number(numRef.current.value) },
+      });
+      numRef.current.value = "";
+    }
+  };
+
+  const resetAll = () => {
+    dispatch({ type: "RESET" });
+    (incRef.current.value = ""),
+      (decRef.current.value = ""),
+      (numRef.current.value = "");
+  };
   return (
     <div>
       <p>Counter: {state.counter}</p>
@@ -89,43 +126,16 @@ const ReducerPage = () => {
       <button onClick={() => dispatch({ type: "DECREMENT_BY_ONE" })}>
         DECREMENT
       </button>
-      <button onClick={() => dispatch({ type: "RESET" })}>RESET</button>
+      <button onClick={resetAll}>RESET</button>
       <br />
       <br />
-      <button
-        onClick={() =>
-          dispatch({
-            type: "INCREMENT",
-            payload: { amount: Number(incRef.current.value) },
-          })
-        }
-      >
-        Inc
-      </button>
+      <button onClick={checkIncrement}>Inc</button>
       <input type="text" placeholder="Increment number" ref={incRef} />
       <br />
-      <button
-        onClick={() =>
-          dispatch({
-            type: "DECREMENT",
-            payload: { amount: Number(decRef.current.value) },
-          })
-        }
-      >
-        Dec
-      </button>
+      <button onClick={checkDecrement}>Dec</button>
       <input type="text" placeholder="Decrement number" ref={decRef} />
       <br />
-      <button
-        onClick={() =>
-          dispatch({
-            type: "SET_COUNTER",
-            payload: { amount: Number(numRef.current.value) },
-          })
-        }
-      >
-        Set
-      </button>
+      <button onClick={checkSet}>Set</button>
       <input type="text" placeholder="Set number" ref={numRef} />
     </div>
   );
